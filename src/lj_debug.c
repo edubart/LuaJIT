028d6580444017fc26e99fdd0d330b55d6275199
Eduardo Bart <edub4rt@gmail.com>
2019-12-28 08:07:22 -0300

    Double the traceback frame limit

--- src/lj_debug.c
+++ src/lj_debug.c
@@ -647,8 +647,8 @@ void lj_debug_dumpstack(lua_State *L, SBuf *sb, const char *fmt, int depth)
 #endif
 
 /* Number of frames for the leading and trailing part of a traceback. */
-#define TRACEBACK_LEVELS1	12
-#define TRACEBACK_LEVELS2	10
+#define TRACEBACK_LEVELS1	24
+#define TRACEBACK_LEVELS2	20
 
 LUALIB_API void luaL_traceback (lua_State *L, lua_State *L1, const char *msg,
 				int level)
